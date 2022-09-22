df979d1a2404b041b828d0157f623538e863abf9
ricky moua <rmoua018@gmail.com>
2022-09-22 01:13:16 -0500

    dom elements were added to jS

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// elements
+var initial = document.querySelector("#initial");
+var questions = document.querySelector("#questions");
+var timer = document.querySelector("#timer");
+var submitButton = document.querySelector("#submitButton");
+var startButton = document.querySelector("#startButton");
+
+var timer = questions.length 
