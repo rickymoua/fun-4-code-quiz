6d9bbf227b4cc231b6fedd6ac16fb58fc74cb262
ricky moua <rmoua018@gmail.com>
2022-09-22 01:47:42 -0500

    quiz question js added to repository

---
+++ js/quiz-questions.js
@@ -0,0 +1,7 @@
+// Quiz Questions
+
+var questions = {
+    title : "what does javaScript stand for?",
+    choices: ["java", "22 language", "test", "code"],
+    Answer: "java"
+}
--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
 // elements
-var initial = document.querySelector("#initial");
-var questions = document.querySelector("#questions");
-var timer = document.querySelector("#timer");
-var submitButton = document.querySelector("#submitButton");
-var startButton = document.querySelector("#startButton");
+var initialE1 = document.querySelector("#initial");
+var questionsE1 = document.querySelector("#questions");
+var timerE1 = document.querySelector("#timer");
+var submitButtonE1 = document.querySelector("#submitButton");
+var startButtonE1 = document.querySelector("#startButton");
+var choiceE1 = document.querySelector("#choice");
 
-var timer = questions.length 
+var timer = questions.length * 20;
+var timerId;
+
+function startQuiz(){
+    var startScreenE1 = document.getElementById("#screen-start");
+    startButtonE1 = setAttribute("class", "hidden");
+
+    questionsE1.removeAttribute("class");
+
+    timerId = setAttribute(tickingClock, 1000);
+    timerE1.textContent = time;
+    Question();
+}
+
+function Question(){
+
+    var curQuestion = questions[crrtQuestionIndex];
+
+    var titleE1 = document.getElementById("title");
+    titleE1 = titleE1.textContent = curQuestion.title;
+    choicesE1.innerHTML = titleE1;
+
+    curQuestion.choices.forEach(function(choice, i) {
+        var choiceE2 = document.createElement("choice");
+    })
